06ac50ce7a6a3ab5d39c985b02bf124d4361d1ca
ahh01 <145448378+ahh01@users.noreply.github.com>
2024-05-06 18:54:23 +0200

    font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        internacional: ["Internacional", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
